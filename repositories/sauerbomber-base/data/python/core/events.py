ef6c7ae6449adffbaa713c4e5a0053f1eb6a3891
Hanack <Hanack@756f4d6d-18b5-4d01-b4fd-6730f0f2117f>
2012-01-23 00:20:43 +0000

    + added python minimus core scripts + added python jabber client (TODO: license)
    git-svn-id: http://svn.nooblounge.net/projects/bomberman/trunk@620 756f4d6d-18b5-4d01-b4fd-6730f0f2117f

---
+++ repositories/sauerbomber-base/data/python/core/events.py
@@ -0,0 +1,107 @@
+import timers
+import asyncore
+import logging
+import sys
+import traceback
+from twisted.internet import reactor
+
+class EventManager:
+	def __init__(self):
+		self.events = {}
+	def connect(self, event, func):
+		try:
+			self.events[event].append(func)
+		except KeyError:
+			self.events[event] = []
+			self.connect(event, func)
+	def trigger(self, eventname, args=()):
+		try:
+			for event in self.events[eventname]:
+				try:
+					event(*args)
+				except:
+					exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+					logging.error('Uncaught exception occured in event handler.')
+					logging.error(traceback.format_exc())
+		except KeyError:
+			pass
+
+class PolicyEventManager(EventManager):
+	def __init__(self):
+		EventManager.__init__(self)
+	def trigger(self, event, args=()):
+		try:
+			for event in self.events[event]:
+					if not event(*args):
+						return False
+
+		except KeyError:
+			return True
+		return True
+
+server_events = EventManager()
+policy_events = PolicyEventManager()
+exec_queue = []
+
+def registerServerEventHandler(event, func):
+	'''Call function when event has been executed.'''
+	server_events.connect(event, func)
+
+class eventHandler(object):
+	'''Decorator which registers a function as an event handler.'''
+	def __init__(self, name):
+		self.name = name
+	def __call__(self, f):
+		self.__doc__ = f.__doc__
+		self.__name__ = f.__name__
+		registerServerEventHandler(self.name, f)
+		return f
+
+def triggerServerEvent(event, args):
+	'''Trigger event with arguments.'''
+	server_events.trigger(event, args)
+
+def registerPolicyEventHandler(event, func):
+	'''Call function when policy event has been executed.'''
+	policy_events.connect(event, func)
+
+class policyHandler(object):
+	'''Decorator which registers a function as a policy event handler.'''
+	def __init__(self, name):
+		self.name = name
+	def __call__(self, f):
+		self.__doc__ = f.__doc__
+		self.__name__ = f.__name__
+		registerPolicyEventHandler(self.name, f)
+		return f
+
+def triggerPolicyEvent(event, args):
+	'''Trigger policy event with arguments.'''
+	return policy_events.trigger(event, args)
+
+def execLater(func, args):
+	'''Call function at a later time with arguments in tuple args.'''
+	exec_queue.append((func, args))
+
+def triggerExecQueue():
+	for event in exec_queue:
+		try:
+			event[0](*event[1])
+		except:
+			exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+			logging.warn('Uncaught exception execLater queue.')
+			logging.warn(traceback.format_exc())
+			logging.warn(traceback.extract_tb(exceptionTraceback))
+	del exec_queue[:]
+
+@eventHandler('reload')
+def onReload():
+	server_events.events.clear()
+
+def update():
+	reactor.runUntilCurrent()
+	reactor.doIteration(0)
+	triggerExecQueue()
+
+reactor.startRunning()
+
---
+++ repositories/sauerbomber-base/data/python/core/plugins.py
@@ -0,0 +1,69 @@
+from ConfigParser import ConfigParser, NoOptionError
+import os
+import sys
+import __builtin__
+import logging
+import sauerbomber
+import core.events
+import core.log # Initialize these before loading plugins
+
+plugins = {}
+paths = [ sauerbomber.pyscriptspath() ]
+config_filename = 'plugin.conf'
+init_modules = sys.modules.keys()
+
+class Plugin:
+	def __init__(self, path, config_path):
+		self.path = path
+		conf = ConfigParser()
+		conf.read(config_path)
+		self.isenabled = True
+		try:
+			self.initmodule = conf.get('Plugin', 'module')
+			self.isenabled = (conf.get('Plugin', 'enable') == 'yes')
+			self.name = conf.get('Plugin', 'name')
+			self.version = conf.get('Plugin', 'version')
+			self.author = conf.get('Plugin', 'author')
+		except NoOptionError:
+			self.isenabled = False
+		del conf
+	def loadModule(self):
+		if self.initmodule and self.isenabled:
+			self.module = __import__(os.path.basename(self.path) + '.' + self.initmodule)
+	def unloadModule(self):
+		if self.isenabled:
+			del self.module
+	def enabled(self):
+		return self.isenabled
+
+def plugin(name):
+	return plugins[name]
+
+def loadPlugins():
+	plugins.clear()
+	logging.info('Loading plugins...')
+	for path in paths:
+		files = os.listdir(path)
+		for file in files:
+			dirpath = path + '/' + file
+			config_path = dirpath + '/' + config_filename
+			if os.path.isdir(dirpath) and os.path.exists(config_path):
+				p = Plugin(dirpath, config_path)
+				if p.isenabled:
+					plugins[p.name] = p
+				else:
+					logging.info('Skipping %s plugin' % file)
+	logging.info('Found %i plugins' % len(plugins.keys()))
+	logging.info('Initializing plugins...')
+	for plugin in plugins.values():
+		plugin.loadModule()
+
+def reloadPlugins():
+	for p in plugins.values():
+		p.unloadModule()
+	core.events.triggerServerEvent('reload', ())
+	for p in plugins.values():
+		p.loadModule()
+
+loadPlugins()
+
