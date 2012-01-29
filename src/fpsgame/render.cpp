ff0b15ba28951d81840ff830ce565c7fc6297130
Hanack <Hanack@756f4d6d-18b5-4d01-b4fd-6730f0f2117f>
2012-01-29 23:50:27 +0000

    * added preparation of vwep for the cannon
    git-svn-id: http://svn.nooblounge.net/projects/bomberman/trunk@677 756f4d6d-18b5-4d01-b4fd-6730f0f2117f

--- src/fpsgame/render.cpp
+++ src/fpsgame/render.cpp
@@ -141,7 +141,7 @@ namespace game
             delay = 1000;
         }
         modelattach a[5];
-        static const char *vweps[] = {"vwep/fist", "vwep/shotg", "vwep/chaing", "vwep/rocket", "vwep/rifle", "vwep/gl", "vwep/pistol", "vwep/gl"}; // TODO: BOMB
+        static const char *vweps[] = {"vwep/fist", "vwep/shotg", "vwep/chaing", "vwep/rocket", "vwep/rifle", "vwep/gl", "vwep/pistol", "vwep/gl"}; // TODO: cannon
         int ai = 0;
         if((!mdl.vwep || d->gunselect!=GUN_FIST) && d->gunselect<=GUN_BOMB)
         {
