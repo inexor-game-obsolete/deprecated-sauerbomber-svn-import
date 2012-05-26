12ad50ce6c0701c7684730abf86afecab26eb95e
Hanack <Hanack@756f4d6d-18b5-4d01-b4fd-6730f0f2117f>
2012-05-26 02:09:34 +0000

    * added dependency injection like mechanism (ultra light weight!) * added unload_plugins * added "base" plugins (server and clients)
    git-svn-id: http://svn.nooblounge.net/projects/bomberman/trunk@833 756f4d6d-18b5-4d01-b4fd-6730f0f2117f

--- repositories/sauerbomber-base/data/python/core/plugins.py
+++ repositories/sauerbomber-base/data/python/core/plugins.py
@@ -6,11 +6,6 @@ import inspect
 import glob
 import string
 import sauerbomber
-
- # Initialize these before loading plugins
-import core.events
-import core.db
-
 from core.config import Config
 
 
@@ -22,6 +17,7 @@ class SauerbomberPlugin(object):
 	name = None
 	config = None
 	context = None
+	requires = []
 
 	def __init__(self, name, config, context):
 		self.name = name
@@ -31,6 +27,18 @@ class SauerbomberPlugin(object):
 
 	def __del__(self):
 		logging.info("Destroyed plugin %s" %(self.name))
+		
+	def require(self, name):
+		'''ultra light weight dependency injection'''
+		setattr(self, name, self.context.plugins[name])
+	
+	def autowire(self):
+		for name in self.requires:
+			self.require(name)
+
+	def init(self):
+		'''ensure that the init method exists and can be overwritten by plugins'''
+		pass
 
 
 class Plugin(object):
@@ -51,24 +59,26 @@ class Plugin(object):
 	def info(self):
 		pass
 		
-	def start(self):
-		logging.info("Trying to start plugin %s" %(self.name))
+	def instantiate(self):
+		logging.debug("Trying to start plugin %s" %(self.name))
 		try:
-			logging.info(sys.path)
 			self.package = __import__(self.name)
 			self.module = sys.modules[self.name]
+			found = False
 			for name, obj in inspect.getmembers(self.module):
-				logging.info("Checking for %s in %s..." %(name, self.module))
+				logging.debug("Checking for %s in %s..." %(name, self.module))
 				if inspect.isclass(obj):
 					if issubclass(obj, SauerbomberPlugin):
 						self.plugin = obj(self.name, self.config, self.context)
-						logging.info("Found plugin class for %s" %(name))
 						break
+			if found:
+				logging.info("Found plugin class for %s" %(name))
+			else:
+				logging.error("Could not find plugin class for %s" %(name))
 		except Exception, e:
-			logging.error("Error: %s"%(e))
 			logging.exception(e)
 
-	def stop(self):
+	def deinstantiate(self):
 		del self.plugin
 		del self.module
 		del self.package
@@ -115,44 +125,49 @@ class PluginManager(object):
 		config = PluginConfig(name)
 		config.load()
 		self.plugins[name] = Plugin(name, config, self)
-		
+
 	def remove(self, name):
 		del self.plugins[name]
-		
+
 	def auto_detect(self):
 		if self.ready and self.config.data.has_key('paths'):
 			for path in self.config.data['paths']:
 				logging.info("Autodetecting plugin modules in %s" %(path))
-				for plugin_type in [ 'core', 'client', 'server' ]:
+				for plugin_type in [ 'base', 'client', 'server' ]:
 					for module_path in glob.glob("%s/%s/*" %(path, plugin_type)):
 						if os.path.isdir(module_path):
 							module = module_path.replace(path, "")[1:].replace("/", ".")
 							self.add(module)
-#				for client_module_path in glob.glob("%s/client/*" %(path)):
-#					if os.path.isdir(client_module_path):
-#						client_module = client_module_path.replace(path, "")[1:].replace("/", ".")
-#						self.add(client_module)
-#				for server_module_path in glob.glob("%s/server/*" %(path)):
-#					if os.path.isdir(server_module_path):
-#						server_module = server_module_path.replace(path, "")[1:].replace("/", ".")
-#						self.add(server_module)
 		else:
 			logging.error("auto detect error")
 
-	def start(self, name):
-		self.plugins[name].start()
-		
-	def stop(self, name):
-		self.plugins[name].stop()
+	def instantiate(self, name):
+		self.plugins[name].instantiate()
 
-	def start_all(self):
-		for name, plugin in self.plugins.items():
-			plugin.start()
+	def deinstantiate(self, name):
+		self.plugins[name].deinstantiate()
+
+	def autowire(self, name):
+		self.plugins[name].autowire()
 		
-	def stop_all(self):
+	def post_init(self, name):
+		self.plugins[name].init()
+
+	def instantiate_all(self):
 		for name, plugin in self.plugins.items():
-			plugin.stop()
-		
+			plugin.instantiate()
+
+	def deinstantiate_all(self):
+		for name, plugin in self.plugins.items():
+			plugin.deinstantiate()
+
+	def autowire_all(self):
+		for name, plugin in self.plugins.items():
+			plugin.autowire()
+
+	def post_init_all(self):
+		for name, plugin in self.plugins.items():
+			plugin.init()
 
 logging.basicConfig(
 	filename = 'repositories/home/logs/python.log',
@@ -167,16 +182,21 @@ def load_plugins():
 	try:
 		plugin_manager.clear()
 		plugin_manager.auto_detect()
-		plugin_manager.start_all()
+		plugin_manager.instantiate_all()
+		plugin_manager.autowire_all()
+		plugin_manager.post_init_all()
 	except Exception, e:
-		logging.error("Error: %s"%(e))
 		logging.exception(e)
 	
 def reload_plugins():
 	try:
-		plugin_manager.stop_all()
-		plugin_manager.clear()
-		plugin_manager.auto_detect()
+		plugin_manager.deinstantiate_all()
+		load_plugins()
 	except Exception, e:
-		logging.error("Error: %s"%(e))
+		logging.exception(e)
+
+def unload_plugins():
+	try:
+		plugin_manager.deinstantiate_all()
+	except Exception, e:
 		logging.exception(e)
