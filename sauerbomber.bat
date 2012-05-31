292926eef230ad33460eb81a9a1065a1d96621e0
westernheld <westernheld@756f4d6d-18b5-4d01-b4fd-6730f0f2117f>
2012-05-31 19:17:29 +0000

    added an if statement to the .bat which selects automatically the right OS type (32 bit or 64 bit) and selects the right folder.
    git-svn-id: http://svn.nooblounge.net/projects/bomberman/trunk@839 756f4d6d-18b5-4d01-b4fd-6730f0f2117f

--- sauerbomber.bat
+++ sauerbomber.bat
@@ -1,1 +1,2 @@
-start bin\sauerbomber.exe -glog.txt -r %* 2>stderr.txt >stdout.txt < NUL
+IF /I "%PROCESSOR_ARCHITECTURE:~-2%"=="64" start bin64\sauerbomber.exe -glog.txt -r %* 2>stderr.txt >stdout.txt < NUL
+IF /I "%PROCESSOR_ARCHITECTURE:~-2%"=="86" start bin\sauerbomber.exe -glog.txt -r %* 2>stderr.txt >stdout.txt < NUL
